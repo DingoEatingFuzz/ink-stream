dcc3467b66b8b6030b7dce447512a7689f8ecd45
Michael Lange <dingoeatingfuzz@gmail.com>
2022-07-08 12:13:35 -0700

    Lockdown the API response a bit
    Throwing raw terminal sequences over http felt funny, so this base64
    encodes chunks as NDJON.

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const path = require('path');
 const React = require('react');
 const importJsx = require('import-jsx');
 const { render } = require('ink');
+const stream = require('stream');
+
+class B64Stream extends stream.Transform {
+  _transform(chunk, enc, next) {
+    this.push(JSON.stringify({ frame: chunk.toString('base64') }) + '\n');
+    next();
+  }
+}
 
 const Counter = importJsx('./counter');
 
@@ -14,6 +22,13 @@ app.listen(4000, () => {
 
 app.get('/logs', (req, res, next) => {
   render(React.createElement(Counter), res);
+});
+
+app.get('/logs-b64', (req, res, next) => {
+  const b64 = new B64Stream();
+  b64.pipe(res);
+
+  render(React.createElement(Counter), b64);
 });
 
 app.get('/', function(req, res) {
